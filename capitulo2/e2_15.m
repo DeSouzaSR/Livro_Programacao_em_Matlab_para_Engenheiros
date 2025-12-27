c990abbcd5d776afc676f7335478bbff948023fa
DeSouzaSR <sandro.fisica@gmail.com>
2025-12-27 10:31:09 -0300

    Fazendo e2_15.m

--- capitulo2/e2_15.m
+++ capitulo2/e2_15.m
@@ -8,7 +8,8 @@
 ##   c) Construir um gráfico do raio de curvatura em função da velocidade, para
 ##      velocidades entre Mach 0.5 e Mach 2.0, assumindo a = 2g
 ##   d) Calcular o menor raio de curvatura para a = 7g e Mach = 1.5
-##   e)
+##   e) Traçar gráfico do raio de curvatura em função da aceleração centrípeta
+##      para acelerações entre 2g e 8g
 
 # Definições
 mach = 340;  # Mach 1 [m/s]
@@ -37,4 +38,8 @@ grid on;
 
 
 # Item d
+v = 1.5 * mach;
+a = 7 * g;
+r = (v ^ 2) / a; # Cálculo do raio de curvatura
+fprintf("Raio de curvatura para v = Mach 1.5  e a = 2g: r = %0.2f m \n", r);
 
